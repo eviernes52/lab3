3e57281be1cf284e80bf58ca87040f2dde42eda0
Eviernes52 <Eviernes52@gmail.com>
2023-08-22 12:02:13 -0700

    finished

--- dbconnect.php
+++ dbconnect.php
@@ -8,7 +8,6 @@ $dbport = 3306;
 
 $conn = new mysqli($host, $dbusername, $dbpassword, $dbname, $dbport);
 
-
 //tests for connection
 if (mysqli_connect_error()) {
     die('Connect Error (' . mysqli_connect_error() . ')');
@@ -37,6 +36,17 @@ if (mysqli_connect_error()) {
         if ($numresults > 0) { //if user already taken "elon is in there"
             $_SESSION['usernameCorrect'] = true;
             $_SESSION['loggedInUsername'] = $toInsertUsername;
+            //we need count right after log in is confirmed to retrive and use count
+            $selectCountQuery = "SELECT count FROM user WHERE username = ?";
+            $stmtSelectCount = $conn->prepare($selectCountQuery);
+            $stmtSelectCount->bind_param("s", $toInsertUsername);
+            $stmtSelectCount->execute();
+            $stmtSelectCount->bind_result($currentCount);
+            $stmtSelectCount->fetch();
+            $stmtSelectCount->close();
+            //create that session for count
+            $_SESSION['countTracker'] = $currentCount;
+            //take back to mainpage.php
             header("Location: mainpage.php");
         } else {
             echo "Invalid username or password";
@@ -44,30 +54,30 @@ if (mysqli_connect_error()) {
         }
         $conn->close();
     }
-
-    //if user not taken
-    $toInsertUsername = $_GET["username"];
-    $toInsertPassword = $_GET["password"];
-
-    $selectQuery = "SELECT username From user WHERE username = ?";
-    $stmt = $conn->prepare($selectQuery);
-    $stmt->bind_param("s", $toInsertUsername);
-    //insertion of user is compelte at '?' character
-    $stmt->execute();
-    $res = $stmt->get_result(); //get result of query
-    $numresults = mysqli_num_rows($res); //OOP or reg function synatx usable
-
-    //check if user already taken
-    if ($numresults > 0) { //if user already taken "elon is in there"
-        echo ("Someone already has that username " . $toInsertUsername);
-        echo '<br><br><a href="lab3.html">Try Again</a>';
-    } else {
-        $insertQuery = "INSERT INTO user (username, password) VALUES (?, ?)";
-        $stmt = $conn->prepare($insertQuery);
-        $stmt->bind_param("ss", $toInsertUsername, $toInsertPassword); //ss for string string
+    //-------------------------------------The PHP code to register a user should be inside an if--------------------------------------
+    if (isset($_POST["registerbutton"])) {
+        //if user not taken
+        $toInsertUsername = $_POST["username"];
+        $toInsertPassword = $_POST["password"];
+        $selectQuery = "SELECT username From user WHERE username = ?";
+        $stmt = $conn->prepare($selectQuery);
+        $stmt->bind_param("s", $toInsertUsername);
+        //insertion of user is compelte at '?' character
         $stmt->execute();
-        echo "New account added successfully";
-        echo '<br><br><a href="lab3.html">Back to home page</a>';
+        $res = $stmt->get_result(); //get result of query
+        $numresults = mysqli_num_rows($res); //OOP or reg function synatx usable
+        //check if user already taken
+        if ($numresults > 0) { //if user already taken "elon is in there"
+            echo ("Someone already has that username " . $toInsertUsername);
+            echo '<br><br><a href="lab3.html">Try Again</a>';
+        } else {
+            $insertQuery = "INSERT INTO user (username, password) VALUES (?, ?)";
+            $stmt = $conn->prepare($insertQuery);
+            $stmt->bind_param("ss", $toInsertUsername, $toInsertPassword); //ss for string string
+            $stmt->execute();
+            echo "New account added successfully";
+            echo '<br><br><a href="lab3.html">Back to home page</a>';
+        }
     }
 
     //increment count if button is clicked
@@ -92,6 +102,7 @@ if (mysqli_connect_error()) {
         //trying to get count for unique identifier (userid)
         //count statement is done
         $stmt->close();
+        //------------------------------When the user logs in, the current count should immediately show (not just after incrementing it)------------------------------------
         // Now we select the count for the userid that is logged in
         $selectQuery = "SELECT count FROM user WHERE userid = ?";
         $selectStmt = $conn->prepare($selectQuery);
