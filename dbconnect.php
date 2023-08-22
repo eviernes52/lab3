0f5a2527c471d174c1e14b0d3f7b753fdcb101f3
Eviernes52 <Eviernes52@gmail.com>
2023-08-22 04:39:06 -0700

    count on log in issues

--- dbconnect.php
+++ dbconnect.php
@@ -6,14 +6,14 @@ $dbpassword = "root";
 $dbname = "lab3";
 $dbport = 3306;
 
-$conn = new mysqli($host, $dbusername, $dbpassword, $dbname, $dbcount, $dbport);
+$conn = new mysqli($host, $dbusername, $dbpassword, $dbname, $dbport);
 
 
 //tests for connection
 if (mysqli_connect_error()) {
     die('Connect Error (' . mysqli_connect_error() . ')');
 } else {
-    echo ("Connection successful <br>");
+    //echo ("Connection successful <br>");
     // after this point, we can use $conn to refer to the connection
     session_start();
 
@@ -58,7 +58,7 @@ if (mysqli_connect_error()) {
     $numresults = mysqli_num_rows($res); //OOP or reg function synatx usable
 
 
-
+    //check if user already taken
     if ($numresults > 0) { //if user already taken "elon is in there"
         echo ("Someone already has that username " . $toInsertUsername);
         echo '<br><br><a href="lab3.html">Try Again</a>';
@@ -71,27 +71,64 @@ if (mysqli_connect_error()) {
         echo '<br><br><a href="lab3.html">Back to home page</a>';
     }
 
+    //increment count if button is clicked
     if (isset($_POST["increment"])) {
-        // Get the user's ID based on the username
-        $selectUserIdQuery = "SELECT userid FROM user WHERE username = ?";
+        $selectUserIdQuery = "SELECT userid FROM user WHERE username = ?"; // USE USERID REMEMBER
         $stmtSelect = $conn->prepare($selectUserIdQuery);
         $stmtSelect->bind_param("s", $_SESSION['loggedInUsername']);
         $stmtSelect->execute();
-        $result = $stmtSelect->get_result();
-        $row = $result->fetch_assoc();
-        $userId = $row['userid'];
+        $result = $stmtSelect->get_result(); //here's the actual result 
+        $row = $result->fetch_assoc();      //needs row to be fetched similar to log 
+        $userId = $row['userid'];           //now userid is stored in variable which cna be used for count
         $stmtSelect->close();
 
-        // Prepare a SQL statement to update the count for the logged-in user
+        // Now we update count for the userid that is logged in
         $updateQuery = "UPDATE user SET count = count + 1 WHERE userid = ?";
         $stmt = $conn->prepare($updateQuery);
         $stmt->bind_param("i", $userId);
         // Execute the prepared statement
         $stmt->execute();
+        // Here I output data of row
+        header("Location: mainpage.php");
+        //trying to get count for unique identifier (userid)
+        echo "Current Count: " . $_SESSION['count'] . "<br>";
+        //count statement is done
         $stmt->close();
 
+
+        $selectQuery = "SELECT count FROM user WHERE userid = ?";
+        $selectStmt = $conn->prepare($selectQuery);
+        $selectStmt->bind_param("i", $userId);
+
+        // Execute the SELECT statement
+        $selectStmt->execute();
+
+        // Bind the result variable
+        $selectStmt->bind_result($updatedCount);
+
+        // Fetch the result
+        $selectStmt->fetch();
+
+        // Close the SELECT statement
+        $selectStmt->close();
+
+        // Store the updated count in a session variable
+        $_SESSION['countTracker'] = $updatedCount;
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Update the session count
-        $_SESSION['count'] = $_SESSION['count'] + 1;
+        //$_SESSION['count'] = $_SESSION['count'] + 1;
     }
 
     $conn->close();
--- mainpage.php
+++ mainpage.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-session_start();
-?>
+
 
 <head>
     <meta charset="UTF-8">
     <title>Logged In</title>
+    <?php session_start(); ?>
 
 </head>
 
 <body>
-    <h1>Welcome Back</h1>
+    <h1>Welcome <?php echo $_SESSION['loggedInUsername']; ?>
+    </h1>
+    <?php
+    if (isset($_SESSION['countTracker'])) {
+        echo "Current Count: " . $_SESSION['countTracker'] . "<br> <br>";
+        // Clear the session variable after displaying the count if needed
+        // unset($_SESSION['updatedCount']);
+    }
+    ?>
     <form name="Button" method="post" action="dbconnect.php">
         <button type="submit" name="increment">increment</button>
     </form>
-    <form name="Logout" method="post" action="dbconnect.php" <br>
+    <form name="Logout" method="post" action="dbconnect.php" <br><br>
         <input type="hidden" name="logout" value="true">
         <button type="submit" name="logout">Logout</button>
     </form>
